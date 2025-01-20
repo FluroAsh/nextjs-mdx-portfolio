b0a8d67b33df7578e3303b623622a49b63c45bec
Ashley Thompson <workashleythompson@gmail.com>
2025-01-20 16:37:38 +1100

    chore: Cleans up redundant aspect-ratio code (Next will already reserve space correctly with defined width/height)

--- src/components/mdx/markdown-image.tsx
+++ src/components/mdx/markdown-image.tsx
@@ -17,11 +17,10 @@ export const MarkdownImage = async ({
   if (!src || !alt)
     throw new Error("Images must include src and alt attributes");
 
-  const { orientation, aspectRatio, width, height, base64 } =
-    await getImagePlaceholder(
-      src,
-      src.includes(env.basePath) ? "large" : undefined,
-    );
+  const { orientation, width, height, base64 } = await getImagePlaceholder(
+    src,
+    src.includes(env.basePath) ? "large" : undefined,
+  );
 
   return (
     <Image
@@ -30,12 +29,10 @@ export const MarkdownImage = async ({
         orientation === "portrait" && "w-[400px]",
         "rounded-sm max-w-full mx-auto transition-opacity",
       )}
-      // Aspect ratio is required for the browser to reserve the correct space for the image
-      // https://www.andreaverlicchi.eu/blog/aspect-ratio-modern-reserve-space-lazy-images-async-content-responsive-design/
-      style={{ aspectRatio }}
       alt={alt}
       width={width as any}
       height={height as any}
+      loading="lazy"
       placeholder="blur"
       blurDataURL={base64}
       // sizes="(max-width: 600px) 750px, 1000px"
