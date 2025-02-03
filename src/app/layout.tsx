8b0228fd2df720a848cfe650dc1eb7a92e9d7bcd
Ashley Thompson <workashleythompson@gmail.com>
2025-02-03 20:59:19 +1100

    chore: Move fonts to /src & root page (homepage) to top level route group

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { catamaran, firaSans } from "./fonts";
+import { catamaran, firaSans } from "../fonts";
 import "@/css/globals.css";
 
 import { cn } from "@/lib/utils";
