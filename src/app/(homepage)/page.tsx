52277db14510a223dc255ab9c9abe81a53eefb16
Ashley Thompson <workashleythompson@gmail.com>
2025-03-02 22:44:17 +1100

    chore: Adds some placeholders for HP timeline

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -29,7 +29,23 @@ export default function Home() {
       </section>
 
       <section className="mx-auto bg-purple-600">
-        <div className="p-8 sm:p-20 max-w-5xl">Timeline</div>
+        <div className="p-8 sm:p-20 max-w-5xl">
+          <h3>Timeline</h3>
+          <ul>
+            <li>
+              Swinburne University - Bachelors of Information Systems (2014 -
+              2017)
+            </li>
+            <li>
+              <a href="https://liquipedia.net/overwatch/Fluro" target="_blank">
+                Overwatch: Semi-Pro Player
+              </a>{" "}
+              (2018 - 2020)
+            </li>
+            <li>CoderAcademy Fullstack Bootcamp (2021 - 2022)</li>
+            <li>MYER - Frontend Engineer (2022 - Current)</li>
+          </ul>
+        </div>
       </section>
 
       {/* TODO: Create some more meaningful full-stack projects */}
