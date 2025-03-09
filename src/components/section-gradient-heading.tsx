d3dd984bf252218f644ca25d4fc2b95a71b013f7
Ashley Thompson <workashleythompson@gmail.com>
2025-03-10 00:04:43 +1100

    refactor: Updates section gradient heading line to be evenly space on mobile

--- src/components/section-gradient-heading.tsx
+++ src/components/section-gradient-heading.tsx
@@ -9,7 +9,7 @@ export const SectionGradientHeading = ({ title, className }: Props) => (
   <h3
     className={cn("mb-6 flex items-center gap-4 text-2xl font-bold", className)}
   >
-    <div className="max-w-[45px] flex-grow">
+    <div className="flex-grow sm:max-w-16">
       <span className="block h-[1px] w-full rounded-full bg-gradient-to-r from-transparent via-green-500/30 to-green-500/70" />
     </div>
 
